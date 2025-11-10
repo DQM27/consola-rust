63608302d49df273e4ff74f9ecd258281593744e
DQM27 <daniel.bleach1@gmail.com>
2025-11-09 22:57:51 -0600

    prueba

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+   
+   saludar_usuario();
+
+}
+
+fn saludar_usuario(){
+    println!("Cual es tu nombre");
+
+    let mut nombre = String::new();
+
+    io::stdin()
+        .read_line(&mut nombre)
+        .expect("Error al leer el nombre");
+
+    let nombre = nombre.trim();
+
+    println! ("Hola {} Eres una zorra Bienvenido a Rust.", nombre);
 }
